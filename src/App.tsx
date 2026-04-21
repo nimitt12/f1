2fd26f81bdb76750c6f98b248537e047811f5f23
Nimitt Ajmera <nimitt.ajmera@taskus.com>
2026-04-21 15:01:03 +0530

    feat: implement dynamic theme switching and global CSS variables for F1 dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -61,6 +61,11 @@ const App: React.FC = () => {
 
   return (
     <>
+      <div className="app-bg">
+        <div className="bg-orb orb-1"></div>
+        <div className="bg-orb orb-2"></div>
+        <div className="bg-orb orb-3"></div>
+      </div>
       <ThemeSwitcher />
       <Ticker />
       <Hero user={user} setUser={setUser} />
